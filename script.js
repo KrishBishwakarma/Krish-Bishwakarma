d4bacb1bd9ee5d7dfe1545e3c423bfeb2e059af7
Krish Bishwakarma <98804037+KrishBishwakarma@users.noreply.github.com>
2022-11-11 20:38:32 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Android Developer", "Web Developer", "Musician", "Freelancer"],
+        strings: ["Android Developer", "Web Developer", "Software Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
